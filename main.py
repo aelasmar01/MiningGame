812eb158f29a7837b2ab0175d4c7aa87a2c1b110
aelasmar01 <101534799+aelasmar01@users.noreply.github.com>
2022-06-17 21:18:06 -0400

    Basic Movement Implemented
    .KEYDOWN Checks for if keys on a keyboard get pressed down 
    .KEYUP Checks for the release of keys
    Everything regarding the player (starting position, speed, change in position[change in position relation])
    By using the changeInPlayerX and changeInPlayerY this allows the player to move by keeping a key pressed down
    By using KEYUP and setting changeInPlayerX and changeInPlayerY to zero when doping so prevents the character from moving across the screen without user input

--- main.py
+++ main.py
@@ -3,27 +3,63 @@ import pygame
 pygame.init()
 
 #Creating screen and initalizing screen size 800 x 600 pixels
-screen = pygame.display.set_mode( (800,600) )
+screenMaxX = 800
+screenMaxY = 600
+screen = pygame.display.set_mode( (screenMaxX,screenMaxY) )
 
 #Game Caption
 pygame.display.set_caption("MotherLoad")
 icon = pygame.image.load("pickaxe.png")
 pygame.display.set_icon(icon)
 
+#Starting Position, Player Img and the speed of the player while moving
 PlayerImg = pygame.image.load("001-miner.png")
 PlayerX = 370
 PlayerY = 270
+PlayerSpeed = 0.4
+changeInPlayerX =0
+changeInPlayerY =0
 
-def Player():
-    screen.blit(PlayerImg,(PlayerX,PlayerY))
+#Draws character
+def Player(x,y):
+    screen.blit(PlayerImg,(x,y))
 
 #Game will only close if close button is pressed
 programRunning = True
+
+
 while programRunning == True:
+
     for event in pygame.event.get():
+        
+        if event.type == pygame.KEYDOWN:
+
+            if event.key == pygame.K_LEFT:
+                changeInPlayerX -= PlayerSpeed
+
+            if event.key == pygame.K_RIGHT:
+                changeInPlayerX += PlayerSpeed
+
+            if event.key == pygame.K_UP:
+                changeInPlayerY -= PlayerSpeed
+
+            if event.key == pygame.K_DOWN:
+                changeInPlayerY += PlayerSpeed
+
+        if event.type == pygame.KEYUP:
+            
+            if event.key == pygame.K_RIGHT or event.key == pygame.K_LEFT:
+                changeInPlayerX = 0
+                
+            if event.key == pygame.K_UP or event.key == pygame.K_DOWN:
+                changeInPlayerY = 0
+
         if event.type == pygame.QUIT:
             programRunning = False
 
+
+    PlayerX += changeInPlayerX
+    PlayerY += changeInPlayerY
     screen.fill((0,255,175))
-    Player()
+    Player(PlayerX,PlayerY)
     pygame.display.update()
